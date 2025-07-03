b2cdaa8e45d1875f3d1aebc480edb6d7da4622b7
Eniyavan <e20099@eng.pdn.ac.lk>
2025-07-03 11:58:59 +0530

    firebase_notification

---
+++ code/BackEnd_MobileApp/Green_Tech/src/main/java/com/Green_Tech/Green_Tech/Config/FireBaseInitializer.java
@@ -0,0 +1,40 @@
+package com.Green_Tech.Green_Tech.Config;
+
+import java.io.FileInputStream;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+//@Configuration
+//public class FireBaseInitializer {
+//		try {
+////			FirebaseOptions file = FirebaseOptions.builder().setCredentials(
+////					GoogleCredentials.fromStream(this.getClass().getClassLoader().getResourceAsStream("fir-fcm-v1-firebase.json")))
+////					.build();
+////
+////			System.out.println("Check File : "+file);
+//
+//        FileInputStream serviceAccount = new FileInputStream();
+//        //System.out.println("Service Acc File: "+serviceAccount);
+//
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+//        //System.out.println("Got IT: "+options);
+//        if(FirebaseApp.getApps().isEmpty()) {
+//            FirebaseApp.initializeApp(options);
+//            System.out.println("Firebase App initialized successfully!");
+//        }else {
+//            System.out.println("Else :Firebase App already initialized.");
+//        }
+//
+//    } catch (Exception e) {
+//        e.printStackTrace();
+//    }
+//}
+//
+//}
